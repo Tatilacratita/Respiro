274064bea51dc42121e5985b9a28c4c661506657
Respiro <tatilacratita@gmail.com>
2024-10-28 02:11:12 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,4 +14,47 @@ document.addEventListener("DOMContentLoaded", function() {
             behavior: "smooth"
         });
     }
+
+    // Scroll animations
+    const scrollElements = document.querySelectorAll(".scroll-element");
+
+    const elementInView = (el, dividend = 1) => {
+        const elementTop = el.getBoundingClientRect().top;
+        return (
+            elementTop <= 
+            (window.innerHeight || document.documentElement.clientHeight) / dividend
+        );
+    };
+
+    const displayScrollElement = (element) => {
+        element.classList.add("scrolled");
+    };
+
+    const handleScrollAnimation = () => {
+        scrollElements.forEach((el) => {
+            if (elementInView(el, 1.25)) {
+                displayScrollElement(el);
+            }
+        })
+    };
+
+    window.addEventListener("scroll", () => {
+        handleScrollAnimation();
+    });
+
+    handleScrollAnimation(); // Trigger the animation on load
+
+    // Sticky navigation
+    const navbar = document.querySelector("nav");
+    const sticky = navbar.offsetTop;
+
+    const stickyNavigation = () => {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    };
+
+    window.addEventListener("scroll", stickyNavigation);
 });
